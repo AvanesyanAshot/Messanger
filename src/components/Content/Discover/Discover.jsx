42a924438df483b97d85885604d8bf077a5363e6
AvanesyanAshot <9teron9@mail.ru>
2020-06-30 15:40:55 +0300

    map discoverData

--- src/components/Content/Discover/Discover.jsx
+++ src/components/Content/Discover/Discover.jsx
@@ -27,6 +27,9 @@ const Discover = () => {
         {id:8,name:'Vitalik'},
         {id:9,name:'Grey'}
     ]
+
+    let newDiscoverData = discoverData.map(item => (<DiscoverItem id={item.id} name={item.name} />))
+
     return (
         <div className={css.content}>
             <aside className={css.nav}>
@@ -36,15 +39,7 @@ const Discover = () => {
                 <br/><br/><br/><br/><br/>
             </aside>
             <div className={css.result}>
-                <DiscoverItem id={discoverData[0].id} name={discoverData[0].name} />
-                <DiscoverItem id={discoverData[1].id} name={discoverData[1].name} />
-                <DiscoverItem id={discoverData[2].id} name={discoverData[2].name} />
-                <DiscoverItem id={discoverData[3].id} name={discoverData[3].name} />
-                <DiscoverItem id={discoverData[4].id} name={discoverData[4].name} />
-                <DiscoverItem id={discoverData[5].id} name={discoverData[5].name} />
-                <DiscoverItem id={discoverData[6].id} name={discoverData[6].name} />
-                <DiscoverItem id={discoverData[7].id} name={discoverData[7].name} />
-                <DiscoverItem id={discoverData[8].id} name={discoverData[8].name} />
+                { newDiscoverData }
             </div>
         </div>
     )
