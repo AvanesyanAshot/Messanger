1d6487b589ffca0a6b79260236dcbc3c1d12ea24
AvanesyanAshot <9teron9@mail.ru>
2020-07-02 17:59:01 +0300

    add store-OOP

--- src/index.js
+++ src/index.js
@@ -2,24 +2,23 @@ import ReactDOM from "react-dom";
 import React from "react";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {addMessage, subscribe, updateNewMessage} from "./psevdoRedux/state";
 // Псевдо REDUX
-import state from './psevdoRedux/state'
+import store from './psevdoRedux/state'
 
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <App state={state}
-                 addMessage={addMessage}
-                 updateNewMessage = {updateNewMessage}
+                 addMessage={store.addMessage.bind(store)}
+                 updateNewMessage = {store.updateNewMessage.bind(store)}
             />
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
-rerenderEntireTree(state)
+rerenderEntireTree(store.getState())
 
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
 
 serviceWorker.unregister();
--- src/psevdoRedux/state.js
+++ src/psevdoRedux/state.js
@@ -1,55 +1,64 @@
 // Псевдо REDUX
-let rerenderEntireTree = () => {
-    console.log("State changed")
-}
 
-let state = {
-    Discover: {
-        discoverData: [
-            {id: 1, name: 'Pasha'},
-            {id: 2, name: 'Kolya'},
-            {id: 3, name: 'Sveta'},
-            {id: 4, name: 'Ahmed'},
-            {id: 5, name: 'Razmik'},
-            {id: 6, name: 'Gevorg'},
-            {id: 7, name: 'Artem'},
-            {id: 8, name: 'Vitalik'},
-            {id: 9, name: 'Grey'}
-        ]
+let store = {
+    _state: {
+        Discover: {
+            discoverData: [
+                {id: 1, name: 'Pasha'},
+                {id: 2, name: 'Kolya'},
+                {id: 3, name: 'Sveta'},
+                {id: 4, name: 'Ahmed'},
+                {id: 5, name: 'Razmik'},
+                {id: 6, name: 'Gevorg'},
+                {id: 7, name: 'Artem'},
+                {id: 8, name: 'Vitalik'},
+                {id: 9, name: 'Grey'}
+            ]
+        },
+        Messages: {
+            message: [
+                {id: 1, name: 'Pasha', time: '10:45', message: 'Hello world'},
+                {id: 2, name: 'Artem', time: '10:42', message: 'Hi'},
+                {id: 3, name: 'Sveta', time: '10:32', message: 'How are u?'},
+                {id: 4, name: 'Katya', time: '11:21', message: 'Thx'},
+                {id: 5, name: 'Vitalik', time: '11:19', message: 'BB'}
+            ],
+            newMessageText: ''
+        }
     },
-    Messages: {
-        message: [
-            {id: 1, name: 'Pasha', time: '10:45', message: 'Hello world'},
-            {id: 2, name: 'Artem', time: '10:42', message: 'Hi'},
-            {id: 3, name: 'Sveta', time: '10:32', message: 'How are u?'},
-            {id: 4, name: 'Katya', time: '11:21', message: 'Thx'},
-            {id: 5, name: 'Vitalik', time: '11:19', message: 'BB'}
-        ],
-        newMessageText: ''
+    _callSubscriber() {
+        console.log("State changed")
+    },
+    getState() {
+        return this._state
+    },
+    addMessage() {
+        let newMessage = {
+            id: 6,
+            name: 'Kolya',
+            time: '11:53',
+            message: this._state.Messages.newMessageText
+        }
+        this._state.Messages.message.push(newMessage)
+
+        this.updateNewMessage('')
+
+
+        debugger;
+
+
+        this._callSubscriber(this._state)
+    },
+    updateNewMessage(newText) {
+        this._state.Messages.newMessageText = newText
+        this._callSubscriber(this._state)
+    },
+    subscribe(observer) {
+        this._callSubscribe = observer //Паттерн Observer
     }
 }
 
-export const addMessage = () => {
-    let newMessage = {
-        id: 6,
-        name: 'Kolya',
-        time: '11:53',
-        message: state.Messages.newMessageText
-    }
-    state.Messages.message.push(newMessage)
-    state.Messages.newMessageText = ''
-    rerenderEntireTree(state)
-}
+export default store
+window.store = store
 
-export const updateNewMessage = (newText) => {
-    state.Messages.newMessageText = newText
-    rerenderEntireTree(state)
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer //Паттерн Observer
-}
-
-window.state = state
-
-export default state
+//Store - OOP
