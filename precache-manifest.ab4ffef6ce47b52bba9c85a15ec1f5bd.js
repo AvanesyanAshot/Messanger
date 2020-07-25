cda15cde5534d146d0873ffdc64b8896746a3a45
AvanesyanAshot <9teron9@mail.ru>
2020-07-25 17:13:31 +0300

    Updates

---
+++ precache-manifest.ab4ffef6ce47b52bba9c85a15ec1f5bd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbce035f93bd28e4c5c73c33b1b7d2b4",
+    "url": "/Messanger/index.html"
+  },
+  {
+    "revision": "ed9a641c33d42157a142",
+    "url": "/Messanger/static/css/4.5b103aa3.chunk.css"
+  },
+  {
+    "revision": "41eeb746e4253a1a1f85",
+    "url": "/Messanger/static/css/5.2b5b2457.chunk.css"
+  },
+  {
+    "revision": "6964067894a504164f39",
+    "url": "/Messanger/static/css/6.e97e3fdc.chunk.css"
+  },
+  {
+    "revision": "b70ebb6c99d67ee533d0",
+    "url": "/Messanger/static/css/main.96d80e67.chunk.css"
+  },
+  {
+    "revision": "2774e9f0e99083c711a4",
+    "url": "/Messanger/static/js/0.af682574.chunk.js"
+  },
+  {
+    "revision": "11b64f26145c4b30c837",
+    "url": "/Messanger/static/js/3.eb177f94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Messanger/static/js/3.eb177f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed9a641c33d42157a142",
+    "url": "/Messanger/static/js/4.cd25312f.chunk.js"
+  },
+  {
+    "revision": "41eeb746e4253a1a1f85",
+    "url": "/Messanger/static/js/5.9a05411d.chunk.js"
+  },
+  {
+    "revision": "6964067894a504164f39",
+    "url": "/Messanger/static/js/6.7d12ab73.chunk.js"
+  },
+  {
+    "revision": "b70ebb6c99d67ee533d0",
+    "url": "/Messanger/static/js/main.91eb0f1f.chunk.js"
+  },
+  {
+    "revision": "0b243d2341a1a7f79a30",
+    "url": "/Messanger/static/js/runtime-main.059902bf.js"
+  },
+  {
+    "revision": "54e9ec5365eeb967838ffd2a35eda50b",
+    "url": "/Messanger/static/media/avatar.54e9ec53.png"
+  },
+  {
+    "revision": "93546541757a8e9a621a8565ef774446",
+    "url": "/Messanger/static/media/loader.93546541.svg"
+  }
+]);
