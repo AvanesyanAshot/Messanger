50aba9da7184594cf014e9a7ff3582d96bc4cf0b
AvanesyanAshot <9teron9@mail.ru>
2020-08-02 17:02:00 +0300

    [Migrating to TypeScript] profileActionCreators userActionCreators

--- src/Redux/Actions/profileActionCreators.ts
+++ src/Redux/Actions/profileActionCreators.ts
@@ -1,6 +1,18 @@
 import {SAVE_PHOTO, SET_USER_PROFILE, SET_USER_STATUS} from "../Reducers/profileReducer"
 import {PhotosType, ProfileType} from "../../types/types"
 
-export const setUserProfile = (profile: ProfileType) => ({type: SET_USER_PROFILE, profile})
-export const setUserStatus = (status: string) => ({type: SET_USER_STATUS, status})
-export const savePhotoSuccess = (photos: PhotosType) => ({type: SAVE_PHOTO, photos})
+type setUserProfileActionType = {
+    type: typeof SET_USER_PROFILE
+    profile: ProfileType
+}
+export const setUserProfile = (profile: ProfileType): setUserProfileActionType => ({type: SET_USER_PROFILE, profile})
+type setUserStatusActionType = {
+    type: typeof SET_USER_STATUS
+    status: string
+}
+export const setUserStatus = (status: string): setUserStatusActionType => ({type: SET_USER_STATUS, status})
+type savePhotoSuccessActionType = {
+    type: typeof SAVE_PHOTO
+    photos: PhotosType
+}
+export const savePhotoSuccess = (photos: PhotosType): savePhotoSuccessActionType => ({type: SAVE_PHOTO, photos})
--- src/Redux/Actions/userActionCreators.ts
+++ src/Redux/Actions/userActionCreators.ts
@@ -1,10 +1,41 @@
-import { SET_USERS, FOLLOW, UNFOLLOW, SET_TOTAL_USER_COUNT, SET_CURRENT_PAGE, TOGGLE_IS_FETCHING, TOGGLE_IS_FOLLOWING_IN_PROGRESS} from "../Reducers/usersReducer"
+import { SET_USERS, FOLLOW, UNFOLLOW,
+    SET_TOTAL_USER_COUNT, SET_CURRENT_PAGE, TOGGLE_IS_FETCHING,
+    TOGGLE_IS_FOLLOWING_IN_PROGRESS} from "../Reducers/usersReducer"
 import { UsersType } from "../../types/types"
 
-export const setUsersAC = (users: UsersType) => ({type: SET_USERS, users})
-export const followSuccess = (userId: number) => ({type: FOLLOW, userId})
-export const unfollowSuccess = (userId: number) => ({type: UNFOLLOW, userId})
-export const setCurrentPage = (page: number) => ({type: SET_CURRENT_PAGE, page})
-export const setTotalUserCount = (num: number) => ({type: SET_TOTAL_USER_COUNT, num})
-export const toggleIsFetching = (isFetching: boolean) => ({type: TOGGLE_IS_FETCHING, isFetching})
-export const toggleIsFollowingInProgress = (isFetching: boolean, userId: number) => ({type: TOGGLE_IS_FOLLOWING_IN_PROGRESS, isFetching, userId})
+type SetUsersACType = {
+    type: typeof SET_USERS
+    users: UsersType
+}
+export const setUsersAC = (users: UsersType): SetUsersACType => ({type: SET_USERS, users})
+type FollowSuccessActionType = {
+    type: typeof FOLLOW
+    userId: number
+}
+export const followSuccess = (userId: number): FollowSuccessActionType => ({type: FOLLOW, userId})
+type UnfollowSuccessActionType = {
+    type: typeof UNFOLLOW
+    userId: number
+}
+export const unfollowSuccess = (userId: number): UnfollowSuccessActionType => ({type: UNFOLLOW, userId})
+type setCurrentPageActionType = {
+    type: typeof SET_CURRENT_PAGE
+    page: number
+}
+export const setCurrentPage = (page: number): setCurrentPageActionType => ({type: SET_CURRENT_PAGE, page})
+type setTotalUserCountActionType = {
+    type: typeof SET_TOTAL_USER_COUNT
+    num: number
+}
+export const setTotalUserCount = (num: number): setTotalUserCountActionType => ({type: SET_TOTAL_USER_COUNT, num})
+type toggleIsFetchingActionType = {
+    type: typeof TOGGLE_IS_FETCHING
+    isFetching: boolean
+}
+export const toggleIsFetching = (isFetching: boolean): toggleIsFetchingActionType => ({type: TOGGLE_IS_FETCHING, isFetching})
+type toggleIsFollowingInProgressActionType = {
+    type: typeof TOGGLE_IS_FOLLOWING_IN_PROGRESS
+    isFetching: boolean
+    userId: number
+}
+export const toggleIsFollowingInProgress = (isFetching: boolean, userId: number): toggleIsFollowingInProgressActionType => ({type: TOGGLE_IS_FOLLOWING_IN_PROGRESS, isFetching, userId})
